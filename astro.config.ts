84caec607c1604fc6ba85f208f47a1e09c91cedb
Maulana Sodiqin <maulanasdqn@gmail.com>
2023-09-09 03:45:49 +0700

    feature: add storyblok integration

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,26 @@
 import { defineConfig } from "astro/config";
 import UnoCSS from "unocss/astro";
+import storyblok from "@storyblok/astro";
+import { loadEnv } from "vite";
+const { STORYBLOK_TOKEN } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
 
 export default defineConfig({
+  image: {
+    domains: ["astro.build"],
+  },
   integrations: [
+    storyblok({
+      accessToken: STORYBLOK_TOKEN,
+      apiOptions: {
+        region: "us",
+      },
+      components: {
+        page: "storyblok/Page",
+        text: "storyblok/Text",
+        image: "storyblok/Image",
+        description: "storyblok/Description",
+      },
+    }),
     UnoCSS({
       injectReset: true,
       preflight: true,
