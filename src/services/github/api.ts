0de29e18c630ab2ddf53f9024b84716b2e7678d2
Maulana Sodiqin <maulanasdqn@gmail.com>
2023-10-29 12:23:31 +0700

    chore: remove unused code

--- src/services/github/api.ts
+++ src/services/github/api.ts
@@ -4,9 +4,8 @@ export const fetchGithub = async (): Promise<TGithubResponse[] | void> => {
   try {
     const response = await fetch(import.meta.env.GITHUB_API_URL);
     const data = await response.json();
-    console.log(data);
     return data;
   } catch (error) {
-    console.error(error);
+    throw error;
   }
 };
