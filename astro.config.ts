b399b24871d0d2b92b89e53b5cab244699259ed2
Maulana Sodiqin <maulanasdqn@gmail.com>
2024-03-12 14:15:27 +0700

    chore: update dependency and restructure storyblok

--- astro.config.ts
+++ astro.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import UnoCSS from "unocss/astro";
 import storyblok from "@storyblok/astro";
 import { loadEnv } from "vite";
-const { STORYBLOK_TOKEN } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
+const env = loadEnv("", process.cwd(), "STORYBLOK");
 
 export default defineConfig({
   image: {
@@ -10,15 +10,14 @@ export default defineConfig({
   },
   integrations: [
     storyblok({
-      accessToken: STORYBLOK_TOKEN,
+      accessToken: env.STORYBLOK_TOKEN,
       apiOptions: {
         region: "us",
       },
       components: {
-        page: "services/storyblok/Page",
-        text: "services/storyblok/Text",
-        image: "services/storyblok/Image",
-        description: "services/storyblok/Description",
+        main: "components/storyblok/main",
+        hero: "components/storyblok/hero",
+        about: "components/storyblok/about",
       },
     }),
     UnoCSS({
