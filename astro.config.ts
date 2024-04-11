dd4606921141d9f6d02b3d936e1bdc66ca9f921f
Maulana Sodiqin <msdqn@outlook.com>
2024-04-11 19:03:53 +0700

    chore: remove storyblok

--- astro.config.ts
+++ astro.config.ts
@@ -1,25 +1,11 @@
 import { defineConfig } from "astro/config";
 import UnoCSS from "unocss/astro";
-import storyblok from "@storyblok/astro";
-import { loadEnv } from "vite";
-const env = loadEnv("", process.cwd(), "STORYBLOK");
 
 export default defineConfig({
   image: {
     domains: ["astro.build"],
   },
   integrations: [
-    storyblok({
-      accessToken: env.STORYBLOK_TOKEN,
-      apiOptions: {
-        region: "us",
-      },
-      components: {
-        main: "components/storyblok/main",
-        hero: "components/storyblok/hero",
-        about: "components/storyblok/about",
-      },
-    }),
     UnoCSS({
       injectReset: true,
     }),
